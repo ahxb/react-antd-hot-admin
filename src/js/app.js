6d22effe9391342ce2e15cbc2e3ee594b91ca191
leo-yang <ahxb521@163.com>
2017-05-27 23:35:23 +0800

    11

--- src/js/app.js
+++ src/js/app.js
@@ -1,23 +1,28 @@
-import React, { Component } from 'react';
-import { Button } from 'antd';
-import { Progress } from 'antd';
+import React, {Component} from 'react';
+import {Button} from 'antd';
+import {Progress} from 'antd';
 import '../css/app.css';
-import { Layout, Menu, Breadcrumb, Icon } from 'antd';
-const { Header, Content, Footer, Sider } = Layout;
+import {Layout, Menu, Breadcrumb, Icon} from 'antd';
+const {Header, Content, Footer, Sider} = Layout;
 const SubMenu = Menu.SubMenu;
 
 class App extends Component {
-    state = {
-        collapsed: false,
-        mode: 'inline',
-    };
-    onCollapse = (collapsed) => {
+    constructor(props) {
+        super(props);
+        this.state = {
+            collapsed: false,
+            mode: 'inline',
+        }
+    }
+
+    onCollapse(collapsed) {
         console.log(collapsed);
         this.setState({
             collapsed,
             mode: collapsed ? 'vertical' : 'inline',
         });
     }
+
     render() {
         let logo = '';
 
@@ -28,13 +33,13 @@ class App extends Component {
                     <Sider
                         collapsible
                         collapsed={this.state.collapsed}
-                        onCollapse={this.onCollapse}
+                        onCollapse={this.onCollapse.bind(this)}
                     >
-                        <div className="logo" />
+                        <div className="logo"/>
                         <Menu theme="dark" mode={this.state.mode} defaultSelectedKeys={['6']}>
                             <SubMenu
                                 key="sub1"
-                                title={<span><Icon type="user" /><span className="nav-text">User</span></span>}
+                                title={<span><Icon type="user"/><span className="nav-text">User</span></span>}
                             >
                                 <Menu.Item key="1">Tom</Menu.Item>
                                 <Menu.Item key="2">Bill</Menu.Item>
@@ -42,31 +47,31 @@ class App extends Component {
                             </SubMenu>
                             <SubMenu
                                 key="sub2"
-                                title={<span><Icon type="team" /><span className="nav-text">Team</span></span>}
+                                title={<span><Icon type="team"/><span className="nav-text">Team</span></span>}
                             >
                                 <Menu.Item key="4">Team 1</Menu.Item>
                                 <Menu.Item key="5">Team 2</Menu.Item>
                             </SubMenu>
                             <Menu.Item key="6">
               <span>
-                <Icon type="file" />
+                <Icon type="file"/>
                 <span className="nav-text">File</span>
               </span>
                             </Menu.Item>
                         </Menu>
                     </Sider>
                     <Layout>
-                        <Header style={{ background: '#fff', padding: 0 }} />
-                        <Content style={{ margin: '0 16px' }}>
-                            <Breadcrumb style={{ margin: '12px 0' }}>
+                        <Header style={{background: '#ccc', padding: 0}}/>
+                        <Content style={{margin: '0 16px'}}>
+                            <Breadcrumb style={{margin: '12px 0'}}>
                                 <Breadcrumb.Item>User</Breadcrumb.Item>
                                 <Breadcrumb.Item>Bill</Breadcrumb.Item>
                             </Breadcrumb>
-                            <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>
+                            <div style={{padding: 24, background: '#fff', minHeight: 360}}>
                                 Bill is a cat.
                             </div>
                         </Content>
-                        <Footer style={{ textAlign: 'center' }}>
+                        <Footer style={{textAlign: 'center'}}>
                             Ant Design ©2016 Created by Ant UED
                         </Footer>
                     </Layout>
