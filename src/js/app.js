f2a0f990edf1d7bd23455b736ee37dc2e1439d5c
leo <ahxb521@163.com>
2017-05-27 18:59:11 +0800

    111

--- src/js/app.js
+++ src/js/app.js
@@ -2,30 +2,75 @@ import React, { Component } from 'react';
 import { Button } from 'antd';
 import { Progress } from 'antd';
 import '../css/app.css';
+import { Layout, Menu, Breadcrumb, Icon } from 'antd';
+const { Header, Content, Footer, Sider } = Layout;
+const SubMenu = Menu.SubMenu;
 
 class App extends Component {
+    state = {
+        collapsed: false,
+        mode: 'inline',
+    };
+    onCollapse = (collapsed) => {
+        console.log(collapsed);
+        this.setState({
+            collapsed,
+            mode: collapsed ? 'vertical' : 'inline',
+        });
+    }
     render() {
         let logo = '';
-        
+
         return (
             <div className="App">
-                <div className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <h2>Welcome to React111111111</h2>
-                    <input type="text"/>
-                    <Button type="primary">Primary</Button>
 
-                </div>
-                <div>
-                        <Progress type="circle" percent={75} />
-                        <Progress type="circle" percent={70} status="exception" />
-                        <Progress type="circle" percent={100} />
-
-                </div>
-
-                <h1>asdfsdf</h1>
-                <h2>adfasdfas</h2>
-                aasdhfaksjdhfh
+                <Layout>
+                    <Sider
+                        collapsible
+                        collapsed={this.state.collapsed}
+                        onCollapse={this.onCollapse}
+                    >
+                        <div className="logo" />
+                        <Menu theme="dark" mode={this.state.mode} defaultSelectedKeys={['6']}>
+                            <SubMenu
+                                key="sub1"
+                                title={<span><Icon type="user" /><span className="nav-text">User</span></span>}
+                            >
+                                <Menu.Item key="1">Tom</Menu.Item>
+                                <Menu.Item key="2">Bill</Menu.Item>
+                                <Menu.Item key="3">Alex</Menu.Item>
+                            </SubMenu>
+                            <SubMenu
+                                key="sub2"
+                                title={<span><Icon type="team" /><span className="nav-text">Team</span></span>}
+                            >
+                                <Menu.Item key="4">Team 1</Menu.Item>
+                                <Menu.Item key="5">Team 2</Menu.Item>
+                            </SubMenu>
+                            <Menu.Item key="6">
+              <span>
+                <Icon type="file" />
+                <span className="nav-text">File</span>
+              </span>
+                            </Menu.Item>
+                        </Menu>
+                    </Sider>
+                    <Layout>
+                        <Header style={{ background: '#fff', padding: 0 }} />
+                        <Content style={{ margin: '0 16px' }}>
+                            <Breadcrumb style={{ margin: '12px 0' }}>
+                                <Breadcrumb.Item>User</Breadcrumb.Item>
+                                <Breadcrumb.Item>Bill</Breadcrumb.Item>
+                            </Breadcrumb>
+                            <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>
+                                Bill is a cat.
+                            </div>
+                        </Content>
+                        <Footer style={{ textAlign: 'center' }}>
+                            Ant Design ©2016 Created by Ant UED
+                        </Footer>
+                    </Layout>
+                </Layout>
             </div>
         );
     }
