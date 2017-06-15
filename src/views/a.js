64d5d533298dfaa055b12f699a3a11caa1ae3f1d
leo-yang <ahxb521@163.com>
2017-06-15 13:37:54 +0800

    11

--- src/views/a.js
+++ src/views/a.js
@@ -1,8 +1,7 @@
 import React, {Component} from 'react';
-import {Button} from 'antd';
-import {Progress} from 'antd';
+
 import '../css/app.css';
-import {Layout, Menu, Breadcrumb, Icon} from 'antd';
+import {Layout, Menu, Breadcrumb, Icon,Row, Col,Progress,Button} from 'antd';
 
 import Aa from './aa';
 
@@ -14,6 +13,9 @@ class a extends Component {
             data:0,
             collapsed: false,
             mode: 'inline',
+            arr:[
+                {name: 'abc'}
+            ],
         }
     }
 
@@ -24,15 +26,47 @@ class a extends Component {
         })
         console.log(11,data);
     }
+    click(i){
 
+        let  arr = this.state.arr.slice();
+
+        arr.splice(i,1)
+
+        this.setState({arr:  arr});
+    }
+    clickarr(){
+        let  arr = this.state.arr.slice();
+        this.setState({
+            data:this.state.data+1,
+        })
+        arr.push({name: 'ccc'+this.state.data});
+        this.setState({arr:  arr});
+
+    }
     render() {
-        let logo = '';
+        let logo = '',arr=this.state.arr;
+        let list = [];
 
+        for (var i = 0; i<arr.length; i++) {
+            list.push(<li key={i} onClick={this.click.bind(this,i)}>{arr[i].name}</li>)
+
+        }
         return (
             <div >
-                <Button>{this.state.data}</Button>
-                    {/*name 传的字符串  child 传的方法用于子组件修改父组件的state状态*/}
-                    <Aa name="leo" child={this.childleo.bind(this)}>1</Aa>
+                <Row>
+                    <Col span={3}>
+                        <ul>
+                            {list}
+                        </ul>
+                    </Col>
+                    <Col span={3}>
+
+                        <Button onClick={this.clickarr.bind(this)}>{this.state.data}</Button>
+                        {/*name 传的字符串  child 传的方法用于子组件修改父组件的state状态*/}
+                        <Aa name="leo" child={this.childleo.bind(this)}>1</Aa>
+                    </Col>
+                </Row>
+
             </div>
         );
     }
